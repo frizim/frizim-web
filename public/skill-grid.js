69dcecfadf1af92587b20642406490984bd63212
frizim <dev@frizim.com>
2025-04-14 20:20:34 +0200

    Stop Astro from inlining the skill grid script

--- public/skill-grid.js
+++ public/skill-grid.js
@@ -1,19 +1,17 @@
 let transition = 0;
 const grid = document.querySelector(".skill-grid");
 document.querySelectorAll(".skill-cell > input").forEach(el => {
-	const inputEl = el as HTMLInputElement;
-
 	grid?.classList.add("script-active");
 	el.addEventListener("change", e => {
-		if(inputEl.checked) {
+		if(el.checked) {
 			e.preventDefault();
 		}
 	});
-	el.parentElement?.addEventListener("transitionend", (e: TransitionEvent) => {
+	el.parentElement?.addEventListener("transitionend", e => {
 		const targetEl = e.target;
 		if(targetEl instanceof HTMLElement && targetEl.classList.contains("toggled") && e.propertyName == "flex-basis") {
 			if(targetEl.classList.contains("toggled")) {
-				inputEl.checked = false;
+				el.checked = false;
 				targetEl.classList.remove("toggled");
 			}
 			else {
