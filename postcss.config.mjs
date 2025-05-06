75f161ce156c0f6e117079d166f024bcaa8bec9f
frizim <dev@frizim.com>
2025-05-06 07:37:00 +0200

    Add PurgeCSS, update dependencies

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,9 +1,13 @@
 import cssnano from "cssnano";
+import { purgeCSSPlugin } from "@fullhuman/postcss-purgecss";
 import autoprefixer from "autoprefixer";
 
 export default {
     plugins: [
         cssnano(),
+        purgeCSSPlugin({
+            content: ['./src/**/*.astro', './src/**/*.ts']
+        }),
         autoprefixer()
     ]
 };
