dafe6e7b9c21761d5aba03d2f5d25426dab2a16e
frizim <dev@frizim.com>
2025-05-25 13:13:51 +0200

    Add delete button to message list

---
+++ src/pages/admin/message/[form]/delete.ts
@@ -0,0 +1,18 @@
+import type { APIRoute } from "astro";
+import { MESSAGES_DIR } from "../../../../config";
+import { existsSync, unlinkSync } from "node:fs";
+
+export const prerender = false;
+
+export const POST: APIRoute = async (ctx) => {
+    if(!ctx.params.form) {
+        return ctx.redirect("/404");
+    }
+
+    const path = MESSAGES_DIR + "/" + ctx.params.form + "/" + (await ctx.request.formData()).get('messageId')?.toString;
+    if(existsSync(path)) {
+        unlinkSync(path);
+    }
+
+    return ctx.redirect("/admin/messages/" + ctx.params.form + "/list");
+};
