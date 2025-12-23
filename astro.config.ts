f9a8bc026cee2bea762c2e9ee4f9e8e0e230a726
frizim <dev@frizim.com>
2025-12-23 01:37:47 +0100

    Pull commit hash and build date from env vars

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
@@ -28,5 +28,25 @@ export default defineConfig({
 
   adapter: node({
     mode: 'middleware'
-  })
+  }),
+
+  env: {
+    schema: {
+      COMMIT_HASH: envField.string({
+        optional: true,
+        default: "1.2.0",
+        context: "server",
+        access: "public",
+        min: 5,
+        max: 10
+      }),
+      BUILD_DATE: envField.number({
+        optional: true,
+        default: Date.now(),
+        context: "server",
+        access: "public",
+        gt: 0
+      })
+    }
+  }
 });
